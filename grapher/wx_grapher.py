a9907280c161ca6d1deaa3a0f999184a2ff98ff4
xim <xim-git@akuma.no>
2011-04-06 21:53:13 -0300

    Fix scrolling in WX

--- grapher/wx_grapher.py
+++ grapher/wx_grapher.py
@@ -20,21 +20,18 @@ def visualize(data):
     win = wx.ScrolledWindow(frame, -1)
 
     figure = graph(data)
-    # The canvas is a «proxy of <Swig Object of type 'wxPanel *' at 0xAddress>»
-    canvas = FigureCanvas(frame, -1, figure)
+    canvas = FigureCanvas(win, -1, figure)
 
-    # If time scale is 20 times longer than number of actors, make it 20 times
-    # wider than it is tall.
+    # Basically: If time scale is 20 times longer than number of actors, make
+    # it 20 times wider than it is tall.
     canvas.SetSize((int(400 / figure.axes[0].get_data_ratio()), 400))
 
     sizer = wx.BoxSizer(wx.VERTICAL)
     sizer.Add(canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
 
-    # Suspect the sizer trick I'm doing is actually just fucking up stuff for
-    # the poor scrollbar project. Will have to ask wxPython devs?
-    frame.SetSizer(sizer)
-    frame.Fit()
-    frame.SetSize(wx.Size(500, 400))
+    win.SetSizer(sizer)
+    win.Fit()
+    win.SetSize(wx.Size(500, 400))
     win.SetScrollbars(20, 20, int(400 / figure.axes[0].get_data_ratio()) / 20, 400 / 20)
 
     frame.Show()
