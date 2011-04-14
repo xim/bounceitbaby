fc300c0e7091e2e60b30593a398f7a246bfd5fbc
xim <xim-git@akuma.no>
2011-04-14 20:51:40 -0300

    Major refactor, lots of TODOs added, more docstrings added, commented the shit out of the code. Would be hard to split in more commits :p

--- grapher/matplot.py
+++ grapher/matplot.py
@@ -22,100 +22,128 @@ class Graph(Figure):
         self._is_linear = linear
         self._coord = XCoordHelper(linear)
 
-        # Note: later we alter figsize according to scale
+        # Note: later we alter figsize according to scale. All numbers here are
+        # guesses and not really used later.
         super(Graph, self).__init__(dpi=90, figsize=(16,6))
         self._axes = self.add_axes((.05, .085, .92, .865))
 
+        # ... Wow, we do a lot of manual housekeeping!
+        # Keep a copy of the arrows so we can pop() for it.
         self._arrows = list(self.arrows)
+        # Remember all the actors we see.
         self._actors = {}
+        # Remember all distinct message types.
         self._msg_types = {}
-        self._max_actor_id = -1
-        self._min_xvalue = 999999
-        self._max_xvalue = 0
-        self._twinticks = []
+        # For remembering the message type at each timestamp. TODO: Kind of
+        # sucks for linear graphs?
+        self._msgtype_ticks = []
+        self._msgtype_labels = []
 
-        self.init_axes_parameters()
+        # Call all functions on the object, in natural order.
+        logging.debug('Iterating %s in %s (linear: %s)' % (data, self, linear))
         self._iterate_data(data)
+        logging.debug('Setting graph parameters')
+        self._add_actors()
         self._set_axes_options()
-
-    def init_axes_parameters(self):
-        if self._is_linear:
-            self._axes.xaxis.set_minor_locator(MaxNLocator(nbins=25))
-            time_format = DateFormatter('%H:%M:%S')
-            self._axes.xaxis.set_major_formatter(time_format)
-        self._axes.set_xlabel('Time')
+        self._set_tick_parameters()
+        logging.debug('%s seems to have exited successfully' % self)
 
     def _iterate_data(self, data):
         for item in data:
-            # TODO: Is this ugly? Maybe not ugly enough to do something about it?
+
+            # Populate the _actors variable in a kind of brute-force way
             for actor in (item.recipient, item.sender):
                 if not actor in self._actors:
-                    self._max_actor_id += 1
-                    self._actors[actor] = self._max_actor_id
+                    self._actors[actor] = len(self._actors)
+            print item
 
+            # Extract x and y values into names that make more sense graph-wize
             x0, y0 = item.sent_time, self._actors[item.sender]
             x1, y1 = (item.rcvd_time or x0), self._actors[item.recipient]
 
+            # Translate X values to sensible scale, includes some bookkeeping
             x0 = self._coord(x0)
             x1 = self._coord(x1)
 
-            if x0 < self._min_xvalue:
-                self._min_xvalue = x0
-            if x1 > self._max_xvalue:
-                self._max_xvalue = x1
-
+            # If the message type hasn't been seen before, assign a style
             if not item.msg_type in self._msg_types:
                 self._msg_types[item.msg_type] = self._arrows.pop()
 
+            # Make the arrow
+            self._make_arrow(item, x0, y0, x1, y1)
+
+    def _make_arrow(self, item, x0, y0, x1, y1):
             arrow, color = self._msg_types[item.msg_type]
-            logging.debug('Adding arrow from (%s, %s) to (%s, %s)' % (
-                    x0, y0, x1, y1))
-
             x_mid, y_mid = (x0 + x1) / 2., (y0 + y1) / 2.
-            if item.port_name:
-                self._axes.add_artist(Text(x_mid - .2, y_mid, item.port_name, rotation='vertical', verticalalignment='center', horizontalalignment='center'))
-            if item.data:
-                self._axes.add_artist(Text(x_mid + .2, y_mid, item.data, rotation='vertical', verticalalignment='center', horizontalalignment='center'))
-
             self._axes.add_patch(FancyArrowPatch(
                     (x0, y0), (x1, y1), linewidth=1.3, color=color,
                     arrowstyle=arrow, mutation_scale=20))
+            # And add text if there was any
+            # TODO: Rotate by degree if this was linear. The arrow should know
+            # its own rotation, right?
+            if item.port_name:
+                self._axes.add_artist(Text(x_mid - .1, y_mid, item.port_name,
+                    rotation='vertical', verticalalignment='center',
+                    horizontalalignment='center'))
+            if item.data:
+                self._axes.add_artist(Text(x_mid + .1, y_mid, item.data,
+                    rotation='vertical', verticalalignment='center',
+                    horizontalalignment='center'))
 
-            self._twinticks.append((x0, item.msg_type))
+            # And do some bookkeeping.
+            self._msgtype_ticks.append(x0)
+            self._msgtype_labels.append(item.msg_type)
 
-    def _set_axes_options(self):
-        """
-        Note: call only after data has been iterated.
-        """
-        y_margin = .1 * self._max_actor_id
-        self._axes.set_ybound(lower=-y_margin, upper=self._max_actor_id + y_margin)
-        x_margin = (self._max_xvalue - self._min_xvalue) * .01
-        self._axes.set_xbound(lower=self._min_xvalue - x_margin, upper=self._max_xvalue + x_margin)
-
-        #self._axes.yaxis.set_visible(False)
-
+    def _add_actors(self):
+        # Add the horizontal lines we want for each actor
         for actor in self._actors:
             a_id = self._actors[actor]
             color = self.colors[a_id % len(self._actors)]
             self._axes.axhline(y=a_id, linewidth=2, color=color, label=actor)
 
-        # Semi-unreadable code making half-guesses for margin size
+    def _set_axes_options(self):
+        """
+        Note: call only after data has been iterated.
+        """
+        # Set axes bounds to include some margins inside the graph
+        max_actor_y = len(self._actors) - 1
+        y_margin = .1 * max_actor_y
+        self._axes.set_ybound(lower=-y_margin, upper=max_actor_y + y_margin)
+        x_margin = self._coord.x_range * .01
+        self._axes.set_xbound(lower=self._coord.min_xvalue - x_margin,
+                upper=self._coord.max_xvalue + x_margin)
+
+        # Semi-unreadable code making half-guesses for margin size outside the
+        # graph. At most .1 in x, at least .16 times the data ratio.
         xmargin = min(.1, .16 * self._axes.get_data_ratio())
         self._axes.set_position((xmargin, .085, 1 - xmargin * 1.8, .865))
 
+        # Activate the nice dotted lines on the tick positions of the x axis
         self._axes.xaxis.grid()
 
-        self._axes2 = self._axes.twiny()
-        xticks, xlabels = zip(*self._twinticks)
-        self._axes2.xaxis.set_ticks(xticks)
-        self._axes2.xaxis.set_ticklabels(xlabels)
-        self._axes2.set_xlim(*self._axes.get_xlim())
+        # Set the figure size, this influences the file export.
+        # TODO: 7 in * 90 px/in = 630 px. Maybe allow the user to set size?
+        self.set_figsize_inches((int(7 / self._axes.get_data_ratio()), 7))
 
-        self._axes.yaxis.set_ticks(self._actors.values())
-        self._axes.yaxis.set_ticklabels(self._actors.keys())
-
-        if not self._is_linear:
+    def _set_tick_parameters(self):
+        if self._is_linear:
+            # If this is a linear graph, make sure it has ticks and timestamps.
+            self._axes.xaxis.set_minor_locator(MaxNLocator(nbins=25))
+            time_format = DateFormatter('%H:%M:%S')
+            self._axes.xaxis.set_major_formatter(time_format)
+        else:
+            # Nonlinear? Then we want to manually determine ticks on x.
             self._axes.xaxis.set_ticks(self._coord.ticks)
             self._axes.xaxis.set_ticklabels(self._coord.labels)
 
-        self.set_figsize_inches((int(7 / self._axes.get_data_ratio()), 7))
+        self._axes.set_xlabel('Time')
+
+        # Add ticks on the top axis with message types printed
+        self._axes2 = self._axes.twiny()
+        self._axes2.xaxis.set_ticks(self._msgtype_ticks)
+        self._axes2.xaxis.set_ticklabels(self._msgtype_labels)
+        self._axes2.set_xlim(*self._axes.get_xlim())
+
+        # Add ticks on the left with the actor names
+        self._axes.yaxis.set_ticks(self._actors.values())
+        self._axes.yaxis.set_ticklabels(self._actors.keys())
--- grapher/utils.py
+++ grapher/utils.py
@@ -2,16 +2,25 @@ from matplotlib.dates import date2num
 from dateutil.parser import parse
 
 def parse_timestamp(timestamp):
+    """
+    Parse a timestamp and return a matplotlib compatible float value
+    """
     return date2num(parse(timestamp))
 
 class XCoordHelper(object):
-    def __init__(self, linear=False, min_xvalue=0, increment=.7):
+    """
+    A class for bookkeeping the x values of linear and nonlinear graphs.
+    """
+    def __init__(self, linear=False, min_xvalue=0, increment=.4):
+        """
+        min_xvalue and increment are discarded if linear=True
+        """
         if linear:
-            self._get_coord = self._get_coord_linear
+            self.get_coord = self._get_coord_linear
         else:
-            self._get_coord = self._get_coord_nonlinear
+            self.get_coord = self._get_coord_nonlinear
         self._odd = False
-        self._x_counter = 0
+        self._max_xvalue = 0
         self._last_x_label = None
         self._min_xvalue = min_xvalue
         self._increment = increment
@@ -19,18 +28,37 @@ class XCoordHelper(object):
         self.ticks = []
 
     def __call__(self, *args):
-        return self._get_coord(*args)
+        """
+        Callable for convenience. Calls self.get_coord (which again calls
+        _get_coord_{non,}linear).
+        """
+        return self.get_coord(*args)
+
+    @property
+    def min_xvalue(self):
+        return self._min_xvalue
+
+    @property
+    def max_xvalue(self):
+        return self._max_xvalue - self._increment
+
+    @property
+    def x_range(self):
+        return self.max_xvalue - self.min_xvalue
 
     def _get_coord_linear(self, value):
         if isinstance(value, basestring):
             value = parse_timestamp(value)
+        if self._max_xvalue == 0:
+            self._min_xvalue = value
+        self._max_xvalue = value
         return value
 
     def _get_coord_nonlinear(self, value=None):
-        new_val = self._min_xvalue + self._x_counter
+        new_val = self._min_xvalue + self._max_xvalue
 
         if self._odd:
-            self._x_counter += self._increment
+            self._max_xvalue += self._increment
         elif self._last_x_label != value:
             self._last_x_label = value
             self.labels.append(value)
