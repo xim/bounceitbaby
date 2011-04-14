06ef92ff0fe6008e409cf2a3aea5f353fe5fc9b2
xim <xim-git@akuma.no>
2011-04-14 20:48:28 -0300

    Fix a major bug caused by not eating superflous whitespace in these logs. I think.

--- importer.py
+++ importer.py
@@ -97,7 +97,7 @@ class CDpp(BaseRegExReader):
     """
     Regex based parser for CD++ logs
     """
-    line_fmt = r'^Me(nsaj|ssag)e (?P<msg_type>\S+)\s+/\s+(?P<sent_time>\S+)\s+/\s+(?P<sender>\S+?)(\(\S+\))?(\s+/\s+(?P<port_name>\S+))??(\s+/\s+(?P<data>\S+))?\s+(para|to)\s+(?P<recipient>\S+?)(\(\S+\))?'
+    line_fmt = r'^Me(nsaj|ssag)e (?P<msg_type>\S+)\s+/\s+(?P<sent_time>\S+)\s+/\s+(?P<sender>\S+?)(\(\S+\))?(\s+/\s+(?P<port_name>\S+))??(\s+/\s+(?P<data>\S+))?\s+(para|to)\s+(?P<recipient>\S+?)(\(\S+\))?\s*$'
 
     def get_data(self, *args, **kwargs):
         for line in super(CDpp, self).get_data(*args, **kwargs):
